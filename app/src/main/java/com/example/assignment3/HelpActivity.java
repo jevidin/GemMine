ea73a6e1a664f007f510a6216e2aa2fa2de5535b
Sean <sean_tam@sfu.ca>
2020-10-16 20:46:20 -0700

    Help page added

---
+++ app/src/main/java/com/example/assignment3/HelpActivity.java
@@ -0,0 +1,27 @@
+package com.example.assignment3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        setupBackButton();
+    }
+
+    private void setupBackButton() {
+        Button btn = findViewById(R.id.btn_back);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
